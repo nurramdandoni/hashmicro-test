99fcab8003befba2229dbea1737e43aceb377c46
Doni Nurramdan <nurramdandoni@gmail.com>
2025-03-11 13:02:10 +0700

    add delete menu

--- product/urls.py
+++ product/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import product_list,product_add, product_create
+from .views import product_list,product_add,product_delete, product_create
 
 urlpatterns = [
     path('products/', product_list, name='product_list'),
     path('products/add', product_add, name='product_add'),
+    path('products/delete/<str:id>/', product_delete, name='product_delete'),
     path('products/create/', product_create, name='product_create'),
 ]
--- product/views.py
+++ product/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Product
 from django.contrib import messages
+from django.http import JsonResponse
 
 def product_list(request):
     products = Product.objects.all()
+    # return JsonResponse({'products': list(products.values())})
     return render(request, 'product/product_list.html', {'products': products})
 
 def product_add(request):
     return render(request, 'product/product_form.html')
+
+def product_delete(request,id):
+    Product.objects.get(id=id).delete()
+    return redirect('product_list')
 
 def product_create(request):
     if request.method == 'POST':
@@ -15,7 +21,14 @@ def product_create(request):
         barcode = request.POST['barcode']
         price = request.POST['price']
         stock = request.POST['stock']
+        # Cek apakah barcode sudah ada
+        if Product.objects.filter(barcode=barcode).exists():
+            messages.error(request, 'Barcode sudah digunakan, gunakan barcode lain.')
+            return render(request, 'product/product_form.html', {'name': name, 'barcode': barcode, 'price': price, 'stock': stock})
+
+        # Jika barcode unik, buat produk baru
         Product.objects.create(name=name, barcode=barcode, price=price, stock=stock)
         messages.success(request, 'Product created successfully!')
+        
         return redirect('product_list')
     return render(request, 'product/product_form.html')
