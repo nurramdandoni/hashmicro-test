84cbd5d1edfbe200538c9647b7e6b0762a1958dd
Doni Nurramdan <nurramdandoni@gmail.com>
2025-03-11 09:41:13 +0700

    add view template install module

--- engine/views.py
+++ engine/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Module
+from django.contrib import messages
 
-# Create your views here.
+def module_list(request):
+    modules = Module.objects.all()
+    return render(request, 'engine/module_list.html', {'modules': modules})
+
+def install_module(request, module_name):
+    module, created = Module.objects.get_or_create(name=module_name)
+    module.installed = True
+    module.save()
+    messages.success(request, f'Module {module_name} installed successfully!')
+    return redirect('module_list')
+
+def uninstall_module(request, module_name):
+    module = Module.objects.get(name=module_name)
+    module.installed = False
+    module.save()
+    messages.success(request, f'Module {module_name} uninstalled successfully!')
+    return redirect('module_list')
