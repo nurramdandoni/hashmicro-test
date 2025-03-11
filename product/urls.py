feb2744940a1c4486ab5317f2445adddf7a5114f
Doni Nurramdan <nurramdandoni@gmail.com>
2025-03-11 16:54:13 +0700

    add edit function

--- product/urls.py
+++ product/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import product_list,product_add,product_delete, product_create
+from .views import product_list,product_add,product_edit,product_delete, product_create,product_update
 
 urlpatterns = [
     path('products/', product_list, name='product_list'),
     path('products/add', product_add, name='product_add'),
     path('products/delete/<str:id>/', product_delete, name='product_delete'),
+    path('products/edit/<str:id>/', product_edit, name='product_edit'),
     path('products/create/', product_create, name='product_create'),
+    path('products/update/', product_update, name='product_update'),
 ]
--- product/views.py
+++ product/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Product
 from django.contrib import messages
 from django.http import JsonResponse
+from django.forms.models import model_to_dict
 
 def product_list(request):
     products = Product.objects.all()
@@ -10,6 +11,12 @@ def product_list(request):
 
 def product_add(request):
     return render(request, 'product/product_form.html')
+
+def product_edit(request,id):
+    products = Product.objects.get(id=id)
+    dataEdit = model_to_dict(products)
+    # return JsonResponse({'products': model_to_dict(products)})
+    return render(request, 'product/product_form_edit.html',{'products': dataEdit})
 
 def product_delete(request,id):
     Product.objects.get(id=id).delete()
@@ -29,6 +36,25 @@ def product_create(request):
         # Jika barcode unik, buat produk baru
         Product.objects.create(name=name, barcode=barcode, price=price, stock=stock)
         messages.success(request, 'Product created successfully!')
+        
+        return redirect('product_list')
+    return render(request, 'product/product_form.html')
+
+def product_update(request):
+    if request.method == 'POST':
+        id = request.POST['id']
+        name = request.POST['name']
+        barcode = request.POST['barcode']
+        price = request.POST['price']
+        stock = request.POST['stock']
+        
+        products = Product.objects.get(id=id)
+        products.name = name
+        products.barcode = barcode
+        products.price = price
+        products.stock = stock
+        products.save()
+        messages.success(request, 'Product Update successfully!')
         
         return redirect('product_list')
     return render(request, 'product/product_form.html')
